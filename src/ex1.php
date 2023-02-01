d8703e731b931c4a573b238b466248de8536df29
Stephen Yang <zhenyu22000@student.hamk.fi>
2023-02-01 10:47:18 +0200

    1

---
+++ src/ex1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task1</title>
+</head>
+<body>
+
+        <?php
+        $title="contact us - hamk - oy";
+        include "first.php"?>
+        <h2> contact us <h2>
+
+
+        ?>
+        <?php
+        <?php
+        
+        $my_string = "Hello my name is Zhenyu, my group is team 6";
+        echo "$my_string Bobettta <br />";
+        echo "Hello world! My name is "David" $my_string <br />";
+        
+        ?>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-8">col-sm-8</div>
+                <div class="col-sm-4">col-sm-4</div>
+            </div>
+            <div class="row">
+                <div class="col-sm">col-sm</div>
+                <div class="col-sm">col-sm</div>
+                <div class="col-sm">col-sm</div>
+            </div>
+        </div>
+
+    
+</body>
+</html>
---
+++ src/firstphp.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+    $txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+    ?>
+
+    <?php
+    $my_string = "Hello World. My name is: ";
+    $my_number = 4;
+    $my_letter = a;
+    echo $my_string;
+    echo $my_number;
+    echo $my_letter;
+    ?>
+    <?php
+    $my_string = "Hello Bob. My name is: ";
+    echo "$my_string Bobettta <br />";
+    echo "Hi, I'm Bob. Who are you? $my_string <br />";
+    echo "Hi, I'm Bob. Who are you? $my_string Bobetta";
+    ?>
+
+</body>
+</html>
+

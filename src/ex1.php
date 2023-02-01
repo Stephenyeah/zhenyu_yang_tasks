b5ac51c94c9ac3157c643e80cf70a0bc421c9afc
Stephen Yang <zhenyu22000@student.hamk.fi>
2023-02-01 12:49:00 +0200

    renew

--- src/ex1.php
+++ src/ex1.php
@@ -1,39 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>task1</title>
 </head>
+<style>
+th, td {
+    background-color: orange;
+}
+table, th, td {
+  border: 1.1px solid black;
+  border-collapse: collapse;
+}
+
+</style>
 <body>
 
+
+
+       <?php include "first.php"?>
+       <h1> 3.1</h1>
+
         <?php
-        $title="contact us - hamk - oy";
-        include "first.php"?>
-        <h2> contact us <h2>
-
-
+        echo "Zhenyu Yang";
+        echo "BBCAP22";
         ?>
-        <?php
+  
+
+        <h1> Current Date </h1>
+
+    
+        <h1> 3.2</h1>
+       <h2> contact us <h2>
         <?php
         
         $my_string = "Hello my name is Zhenyu, my group is team 6";
         echo "$my_string Bobettta <br />";
-        echo "Hello world! My name is "David" $my_string <br />";
+        echo "Hello world! My name is ";
         
         ?>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-sm-8">col-sm-8</div>
-                <div class="col-sm-4">col-sm-4</div>
-            </div>
-            <div class="row">
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-            </div>
-        </div>
+        <h1> Current Date </h1>
+
+        <h1> 3.4 Variable in Php </h1>
+
+        <?php
+        $title1="contact us - hamk - oy";
+        echo "<h1" . $title . "</h1>";        
+        
+        ?>
+        <h1> Current Date </h1>
+
+        <h1> 3.5 table in Php </h1>
+
+        <?php
+        $g1 = 5;
+        $g2 = 4;
+        $g3 =5;
+        
+       echo "
+        <table>
+        <tr>
+        <th> S.N </th> <th> Name </th><th> Grade </th>
+        </tr>
+        <tr>
+        <td> 1 </td><td> Pekka </td><td> $g1 </td>
+        </tr>
+        <tr>
+        <td> 2 </td><td> Johanna </td><td> $g2 </td>
+        <tr>
+        <tr>
+        <td> 3 </td><td> Johanna </td><td> $g3 </td>
+        <tr>
+        </table>"
+            
+        ?>
+        
+        
+        <h1> 4 Screenshoot for the development enviroment </h1>
+        
+        <img src="images/screenshoots.png" alt="wrong" style="width: 70%;">
+        
+       
+
 
     
 </body>
